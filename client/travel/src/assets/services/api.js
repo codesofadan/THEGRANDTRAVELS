94e78b5d710ac504eb7431967af2b6e8bf875afb
Adan <codesofadan@gmail.com>
2025-02-09 14:38:08 +0500

    update API base URL to use environment variable

--- client/travel/src/assets/services/api.js
+++ client/travel/src/assets/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = "https://thegrandtravelsbackend.vercel.app/api";
+const API_BASE = process.env.REACT_APP_API_URL;
 
 // Function to fetch the latest popup image
 export const fetchPopupImage = async () => {
