b28598e23ae82a3cec595b5ab0f60848157d3c8c
Adan <codesofadan@gmail.com>
2025-02-12 11:04:16 +0500

    chore: remove Rollup configuration and update Vercel and Vite settings for build output

--- Admin-Dashboard/vite.config.ts
+++ Admin-Dashboard/vite.config.ts
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist', // Ensure the output directory is set to 'dist'
     chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
