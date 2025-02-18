ff0f98ab47c71fbe3b0b4bbe8d2ac2060bee6842
Adan <codesofadan@gmail.com>
2025-02-18 17:57:50 +0500

    chore: update API_BASE URL to production server

--- client/src/assets/services/api.js
+++ client/src/assets/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://thegrandtravels-server.vercel.app/api";
 
 // Function to fetch the latest popup image
 export const fetchPopupImage = async () => {
